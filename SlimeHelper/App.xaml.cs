28f019782b4cccdbce18a60a3560e10d222dc948
M-Renerg <mikael.k.renberg@hotmail.com>
2026-02-04 22:21:07 +0100

    fixed shotdown for slime

--- SlimeHelper/App.xaml.cs
+++ SlimeHelper/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Windows;
 
 namespace SlimeHelper
@@ -9,6 +10,37 @@ namespace SlimeHelper
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            string[] args = Environment.GetCommandLineArgs();
+
+            if (args.Length > 1)
+            {
+                if (int.TryParse(args[1], out int parentPid))
+                {
+                    try
+                    {
+                        Process parentProcess = Process.GetProcessById(parentPid);
+
+                        parentProcess.EnableRaisingEvents = true;
+
+                        parentProcess.Exited += (s, evt) =>
+                        {
+                            this.Dispatcher.Invoke(() =>
+                            {
+                                Shutdown();
+                            });
+                        };
+                    }
+                    catch (Exception)
+                    {
+                        Shutdown();
+                    }
+                }
+            }
+        }
     }
 
 }

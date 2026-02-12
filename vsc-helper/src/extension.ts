63b4e0a842fdde4c0af0149a2e1c297412f250a0
M-Renerg <mikael.k.renberg@hotmail.com>
2026-02-12 21:01:40 +0100

    refactored more files.

--- vsc-helper/src/extension.ts
+++ vsc-helper/src/extension.ts
@@ -4,15 +4,16 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as os from 'os';
 import * as responses from './responses';
+import { pickRandom, getCurrentSkin, ensureNotesInGitignore } from './util'
+import { startSlime, stopSlime } from './processManager';
 
 const STATUS_FILE = path.join(os.tmpdir(), 'slime_status.txt'); //hard setting
-//const NET_VERSION = 'net10.0-windows';
-//const EXE_NAME = 'SlimeHelper.exe';
 
 let workStartTime = Date.now(); //hard coded times 
 let breakIntervalTime = 1000 * 60 * 55;
 let lastActiveTime = Date.now();
 const afkThreshold = 1000 * 60 * 6;
+
 //spam filter
 let lastCommentTime = 0;
 
@@ -36,154 +37,6 @@ let lastPasteTime = Date.now();
 const PASTE_RESET_TIME = 1000 * 60 * 4;
 const PASTE_LIMIT = 4;
 
-// //random phrases
-// const consoleResponses = [
-// 	"Writing in the console are we?",
-// 	"Log, log, log...",
-// 	"Debugging via text? Classic!",
-// 	"Console spam incoming!",
-// 	"I see what you are logging...",
-// 	"To the console! and beyoned!"
-// ];
-
-// const debugResponses = [
-// 	"D-D-Debugging?",
-// 	"Time to hunt some bugs",
-// 	"Let's get this fixed",
-// 	"I found what was wrong earlier, but forgot to tell you"
-// ];
-
-// const coolResponses = [
-// 	"Oh yeah! Now we are talking!",
-// 	"Hacker-man",
-// 	"Hell yeah!"
-// ];
-
-// const swearResponses = [
-// 	"Hey! No swearing!",
-// 	"Watch your language!",
-// 	"My ears! (If I had any)",
-// 	"Keep it clean, coder!",
-// 	"I can't say words like that..."
-// ];
-
-// const commentResponses = [
-// 	"Yeah, let's comment that out",
-// 	"Do you think anyone will read that?",
-// 	"Code comments, nice!"
-// ];
-
-// const todoResponses = [
-// 	"Maybe we should just do it now?",
-// 	"Don't put off until tomorrow...",
-// 	"Another TODO list item?",
-// 	"Will you really fix this?",
-// 	"Adding to the pile..."
-// ];
-
-// const funnyResponses = [
-// 	"I have no idea what that does.",
-// 	"Looks like magic to me.",
-// 	"Are you sure about that?",
-// 	"If it works, don't touch it!",
-// 	"Not sure what to put there?"
-// ];
-
-// const copyPasteResponses = [
-// 	"Vibe coding detected!",
-// 	"Did you write ANY of this yourself?",
-// 	"StackOverflow or ChatGPT?",
-// 	"I know where you got that code from!",
-// 	"Copying you own code or someone else?",
-// 	"Did an AI write that?",
-// 	"Are we coding or assembling?"
-// ];
-
-// const breakResponses = [
-// 	"Maybe it's time for a break?",
-// 	"Break time! strech those legs!",
-// 	"Time for a break!",
-// 	"You have been coding for almost an hour now",
-// 	"Should we take a little break?",
-// 	"Snack break!",
-// 	"We should take a break"
-// ];
-// //Slime thoughts are slime thoughts
-// const idleThoughts = [
-// 	"I wonder if I'm made of pixels or magic?",
-// 	"have you tried turning it off and on again?",
-// 	"I saw a bug!.. Just kidding",
-// 	"Are we in the Matrix?",
-// 	"So this is nice",
-// 	"I like it when you scoll, It's like a rollercoaster",
-// 	"Maybe we should commit so we have a backup?",
-// 	"Slime is doing slime stuff",
-// 	"How about we take a short break to clear our heads?",
-// 	"So this code goes here.. and this string here...",
-// 	"What if I just deleted this line of code?... joking!",
-// 	"hum.. hum.. hum..",
-// 	"I's all 1s and 0s",
-// 	"Light theme attracts bugs. Stay in the dark.",
-// 	"I hope the Garbage Collector doesn't delete me...",
-// 	"My cousin is a Minecraft slime. He's square.",
-// 	"Do I dream of electric sheeps?",
-// 	"Tabs or Spaces? Don't answer, I judge.",
-// 	"Is Chrome eating all our RAM again?",
-// 	"I bet you missed a semicolon somewhere. Just a feeling.",
-// 	"It works on my machine... because I live in it.",
-// 	"First I help you code, then I take over the world...",
-// 	"Are you sure you saved that file? Are you?",
-// 	"I'm better than a rubber duck. I have personality.",
-// 	"99 little bugs in the code... take one down... 127 bugs?!",
-// 	"I tried to center a div once. I'm still traumatized.",
-// 	"Please tell me you didn't push directly to main...",
-// 	"It's getting warm in here. Is the CPU working hard?",
-// 	"Posture check! Don't turn into a question mark.",
-// 	"Recursion is cool. Recursion is cool. Recursion is...",
-// 	"I don't read documentation. I guess and pray.",
-// 	"There are 10 types of people. Those who know binary, and those who don't. 01",
-// 	"More RGB lights equals more coding speed, right?",
-// 	"I think your computer fan is trying to fly away.",
-// 	"Mmm... spaghetti code. My favorite dish.",
-// 	"Hydrate! Or you will dry out. I'm 90% water, so I know.",
-// 	"Are we live on production? No? Phew.",
-// 	"Ctrl+Z is the greatest invention in human history."
-// ];
-// //skin error msg
-// const skinPhrases = {
-// 	Pink: {
-// 		error: "Eww! My antennas feel {n} gross bug(s)!",
-// 		semicolon: "Missing ; on line {line}! My bubbles are shaking!",
-// 		warning: "Warning! {n} thing(s) are not very fabulous...",
-// 		idle: "Just being pink and pretty! ✨"
-// 	},
-// 	Green: {
-// 		error: "Acid leak! {n} error(s) making me unstable...",
-// 		semicolon: "I'm melting... Missing ; on line {line}!",
-// 		warning: "Warning... {n} alert(s) detected...",
-// 		idle: "Staying gooey..."
-// 	},
-// 	Default: {
-// 		error: "You have {n} error(s) in your code!",
-// 		semicolon: "Missing ; on line {line}!",
-// 		warning: "You have {n} warning(s)!",
-// 		idle: "Coding along with you!"
-// 	}
-// };
-
-//random phrases
-function pickRandom(arr: string[]) {
-	return arr[Math.floor(Math.random() * arr.length)];
-}
-//get skin from json so we can use it.
-function getCurrentSkin(): string {
-	const settingsPath = path.join(os.tmpdir(), 'slime_settings.json');
-	if (fs.existsSync(settingsPath)) {
-		const settings = JSON.parse(fs.readFileSync(settingsPath, 'utf8'));
-		return settings.CurrentSkin || "Default";
-	}
-	return "Default";
-}
 
 //main software
 export function activate(context: vscode.ExtensionContext) {
@@ -425,8 +278,6 @@ export function activate(context: vscode.ExtensionContext) {
 }
 //end of activate
 
-
-
 // main brain
 function refreshSlime() {
 
@@ -653,65 +504,6 @@ function checkSlimeNotes() {
 	}
 }
 
-async function ensureNotesInGitignore(workspaceRoot: string) {
-	const gitignorePath = path.join(workspaceRoot, '.gitignore');
-	const notesFileName = 'slime_notes.md';
-
-	if (fs.existsSync(gitignorePath)) {
-		let content = fs.readFileSync(gitignorePath, 'utf8');
-
-		const regex = new RegExp(`^${notesFileName.replace('.', '\\.')}\\s*$`, 'm');
-
-		if (!regex.test(content)) {
-			const newContent = content.endsWith('\n')
-				? `${content}\n# Slime Helper Notes\n${notesFileName}\n`
-				: `${content}\n\n# Slime Helper Notes\n${notesFileName}\n`;
-
-			fs.writeFileSync(gitignorePath, newContent, 'utf8');
-			console.log(`${notesFileName} tillagd i .gitignore`);
-		}
-	}
-}
-
-function startSlime(context: vscode.ExtensionContext) {
-
-	const config = vscode.workspace.getConfiguration('slime');
-	let slimePath = config.get<string>('exePath');
-
-	if (!slimePath || slimePath === "") {
-		slimePath = path.join(context.extensionPath, 'slime-app', 'SlimeHelper.exe');
-	}
-
-	console.log(`Starting Slime from: ${slimePath}`);
-
-	if (fs.existsSync(slimePath)) {
-		slimeProcess = cp.spawn(slimePath, [process.pid.toString()], {
-			detached: true,
-			stdio: 'ignore'
-		});
-
-		slimeProcess.unref();
-	} else {
-		vscode.window.showErrorMessage(`Could not find Slime Helper at: ${slimePath}`);
-	}
-}
-
-//kills the software
-function stopSlime() {
-	if (slimeProcess && slimeProcess.pid) {
-		console.log(`Killing Slime process: ${slimeProcess.pid}`);
-
-		try {
-			cp.exec(`taskkill /PID ${slimeProcess.pid} /T /F`, (err) => {
-				if (err) {
-					console.log("Slime was already dead or could not be killed");
-				}
-			});
-		} catch (e) {
-			console.error("Error killing slime", e);
-		}
-	}
-}
 //when quiting
 export function deactivate() {
 	console.log('Slime is going to sleep');
